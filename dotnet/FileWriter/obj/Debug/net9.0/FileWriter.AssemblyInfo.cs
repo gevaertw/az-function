123b3b9bcbbdbcda2bd374d5f8fc2655da2d0f5f
wouter <you@example.com>
2025-10-10 09:23:52 +0200

    10/10/2025

--- dotnet/FileWriter/obj/Debug/net9.0/FileWriter.AssemblyInfo.cs
+++ dotnet/FileWriter/obj/Debug/net9.0/FileWriter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileWriter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+691778b09d53b6bcfc58a08442e386958bebc510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9230845c81a98322ef187bb7e03d8c99077f6ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileWriter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileWriter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
